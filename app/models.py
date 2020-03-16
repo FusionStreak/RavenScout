161dc909bf6da836ee045e06765470c6399f00fd
DanielAgapov <53918934+DanielAgapov@users.noreply.github.com>
2020-03-15 22:09:33 -0400

    beginning of chapter 8 default code

--- app/models.py
+++ app/models.py
@@ -1,23 +1,35 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from app import db
-
-
-class User(db.Model):
+from flask_login import UserMixin
+from app import login
+from hashlib import md5
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
-
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+    )
     def __repr__(self):
         return '<User {}>'.format(self.username)
     def set_password(self,password):
         self.password_hash = generate_password_hash(password)
     def check_password(self,password):
         return check_password_hash(self.password_hash,password)
-
-
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+    followed = db.relationship(
+        'User', secondary=followers, 
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -26,3 +38,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
