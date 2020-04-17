551dbf07f2a44d895d90c607abdfb56ea73fcaa3
DanielAgapov <53918934+DanielAgapov@users.noreply.github.com>
2020-04-17 15:00:55 -0400

    needed to add title argument for profile

--- app/routes.py
+++ app/routes.py
@@ -77,7 +77,7 @@ def user(username):
     prev_url = url_for('user', username=user.username, page=posts.prev_num) \
         if posts.has_prev else None
     return render_template('user.html', user=user, posts=posts.items,
-                           next_url=next_url, prev_url=prev_url) 
+                           next_url=next_url, prev_url=prev_url, title='Profile |') 
 
 @app.before_request
 def before_request():
