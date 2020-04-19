c13b18cbb14f7ee1237930577bc85d2ac76d9a9a
Sayfullah Eid <sayfullaheid@gmail.com>
2020-04-19 18:34:40 -0400

    Re-added SQLAlchemy migrations

---
+++ migrations/versions/9c986b0d6d4b_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 9c986b0d6d4b
+Revises: 
+Create Date: 2020-04-19 18:33:36.242863
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9c986b0d6d4b'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
